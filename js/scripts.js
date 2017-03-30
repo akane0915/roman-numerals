61a71a2655008eafa989f3afb292c92024365ec8
Asia Kane and Tanner Eustice <asia@>
2017-03-30 11:26:16 -0700

    add logic to make all specs pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
 //Business Logic
-var romans1to9 = ["I","II","III","IV","V","VI","VII","VIII","IX"];
-var romansTensPlace = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
-
+var romans1to9 = ["","I","II","III","IV","V","VI","VII","VIII","IX"];
+var romansTensPlace = ["","X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+var romansHundredsPlace = ["","C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+var romansThousandsPlace = ["","M", "MM", "MMM"];
 // Alert if userInput >= 4000
 var inputTooBig = function(userInput) {
   if(parseInt(userInput) > 3999) {
@@ -12,23 +13,41 @@ var inputTooBig = function(userInput) {
 
 // Function for one digit input to return the corresponding roman numeral
 var inputOneDigit = function(inputArray){
-  for(i = 0; i <= romans1to9.length; i++){
+  for(i = 0; i <= romans1to9.length - 1; i++){
     if(inputArray[inputArray.length - 1] === i.toString()){
-      return romans1to9[i - 1];
+      return romans1to9[i];
     }
   }
 };
 
 // Function for two digit input to return the corresponding roman numeral
 var inputTwoDigit = function(inputArray){
-  for(i = 0; i <= romansTensPlace.length; i++){
+  for(i = 0; i <= romansTensPlace.length - 1; i++){
     if(inputArray[inputArray.length - 2] === i.toString()){
-      return romansTensPlace[i - 1] + inputOneDigit(inputArray);
+      return romansTensPlace[i] + inputOneDigit(inputArray);
     }
   }
 };
 
-// Breaks userInput string into an array, determines length of array.  If array length is 1 or 2, return the value from function inputOneDigit and inputTwoDigit.
+// Function for three digit input to return the corresponding roman numeral
+var inputThreeDigit = function(inputArray){
+  for(i = 0; i <= romansHundredsPlace.length - 1; i++){
+    if(inputArray[inputArray.length - 3] === i.toString()){
+      return romansHundredsPlace[i] + inputTwoDigit(inputArray);
+    }
+  }
+};
+
+// Function for four digit input to return the corresponding roman numeral
+var inputFourDigit = function(inputArray){
+  for(i = 0; i <= romansThousandsPlace.length - 1; i++){
+    if(inputArray[inputArray.length - 4] === i.toString()){
+      return romansThousandsPlace[i] + inputThreeDigit(inputArray);
+    }
+  }
+};
+
+// Breaks userInput string into an array, determines length of array.  If array length is 1 - 4, return the value from function inputOneDigit, inputTwoDigit, inputThreeDigit, inputFourDigit.
 var arrayLength = function (userInput) {
   var inputArray =  userInput.split("");
   if(inputArray.length === 1){
@@ -36,6 +55,10 @@ var arrayLength = function (userInput) {
   }
   else if(inputArray.length === 2){
     return inputTwoDigit(inputArray);
+  } else if(inputArray.length === 3){
+    return inputThreeDigit(inputArray);
+  } else {
+    return inputFourDigit(inputArray);
   }
 };
 
