3cb02b716a7eabff2143cf0a79435ec857b61442
Asia Kane and Tanner Eustice <asia@>
2017-03-30 11:05:11 -0700

    add logic to assign tens place roman numeral for two digit numbers

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
 //Business Logic
+var romans1to9 = ["I","II","III","IV","V","VI","VII","VIII","IX"];
+var romansTensPlace = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
 
 // Alert if userInput >= 4000
 var inputTooBig = function(userInput) {
@@ -8,18 +10,32 @@ var inputTooBig = function(userInput) {
 };
 
 
-// Function for one digit input to return...
+// Function for one digit input to return the corresponding roman numeral
 var inputOneDigit = function(inputArray){
-  if (inputArray[0] === "2"){
-    return "II";
-  };
-}
+  for(i = 0; i <= romans1to9.length; i++){
+    if(inputArray[0] === i.toString()){
+      return romans1to9[i - 1];
+    }
+  }
+};
 
-// Breaks userInput string into an array, determines length of array.  If array length is 1, return the value from function inputOneDigit.
+// Function for two digit input to return the corresponding roman numeral
+var inputTwoDigit = function(inputArray){
+  for(i = 0; i <= romansTensPlace.length; i++){
+    if(inputArray[0] === i.toString()){
+      return romansTensPlace[i - 1];
+    }
+  }
+};
+
+// Breaks userInput string into an array, determines length of array.  If array length is 1 or 2, return the value from function inputOneDigit and inputTwoDigit.
 var arrayLength = function (userInput) {
   var inputArray =  userInput.split("");
   if(inputArray.length === 1){
     return inputOneDigit(inputArray);
+  }
+  else if(inputArray.length === 2){
+    return inputTwoDigit(inputArray);
   }
 };
 
